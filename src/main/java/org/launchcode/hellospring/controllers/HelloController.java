221e8c5afa185cb8dd87c82d3728ae12b68a636d
Doyle Brown <dalpiad@gmail.com>
2024-01-07 13:47:56 -0600

    Completed the chapter exercises with bonus mission.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,27 +5,30 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("hello")
+@RequestMapping(method = {RequestMethod.POST}, value = "hello")
 public class HelloController {
-    //handle request at path http://localhost:8080/hello
-
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
 
     //handle request at path //http://localhost:8080/hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
+//    @GetMapping("goodbye")
+//    public String goodbye(){
+//        return "Goodbye, Spring!";
+//    }
 
     //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name){
+//        return "Hello, " + name + "!";
+//    }
+
+    @GetMapping()
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name.isEmpty()) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
     }
+
 
     //handle requests of the form http://localhost:8080/hello/LaunchCode
     @GetMapping("{name}")
@@ -40,10 +43,42 @@ public class HelloController {
                 "<body>" +
                 "<form action = '/hello' method = 'post'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
+                "<select name='language' id='language-select'>" +
+                    "<option value='english'>English</option>" +
+                    "<option value='french'>French</option>" +
+                    "<option value='spanish'>Spanish</option>" +
+                    "<option value='german'>German</option>" +
+                    "<option value='italian'>Italian</option>" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+
+    public static String createMessage(String name, String language) {
+        String greeting = "";
+
+        if (language.equals("english")) {
+            greeting = "Hello";
+        } else if (language.equals("french")) {
+            greeting = "Bonjour";
+        } else if (language.equals("spanish")) {
+            greeting = "Hola";
+        } else if (language.equals("german")) {
+            greeting = "hallo";
+        } else if (language.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+
+        //return greeting + " " + name + "!";
+        return "<html>" +
+                "<body>" +
+                "<center><div>" +
+                "<center><h2>" + greeting + " " + name + "!" +
+                "<br><br>Have a wonderful day!</h2></center>" +
+                "</body>" +
+                "<html>";
     }
 
 }
